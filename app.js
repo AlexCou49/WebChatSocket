446bc70fc759731f8b1d1ca7e4b65c600f2f7061
Alexandre Coutant <alexcoutant3@gmail.com>
2020-03-23 16:38:05 +0100

    connect disconnect user , init socketio

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
+//modules
 let express = require('express')
-let app = express()
+let http = require('http')
+let socketio = require('socket.io')
+let morgan = require('morgan')
+let config = require('./config')
 
-app.use((req, res, next) => {
-    console.log('url: ' + req.path)
-    next()
+//variable globale
+const app = express()
+const server = http.Server(app)
+const io = socketio(server)
+const port = config.express.port
+const options = {
+    root: __dirname+ '/views'
+}
+//middlewares
+app.use(express.static(options.root))
+app.use(morgan('dev'))
+
+//Routes
+app.get('/', (req, res) => {
+  res.redirect('./home')
 })
 
-
 app.get('/home', (req, res) => {
-    res.send('hello')
+    res.sendFile('index.html', options)
 })
 
 app.get('/params/:name', (req, res) => {
     res.send(req.params.name)
 })
 
-app.listen('8080', () => { console.log('server started') })
+//IO
+io.on('connection', function(socket){
+
+    console.log('a user connected : ' + socket.id);
+
+    setTimeout(() =>{
+        socket.emit('salut')
+    }, 1000);
+
+//Deconnexion de l'utilisateur
+    socket.on('disconnect', () => {
+        console.log('disconnect : ' + socket.id)
+    })
+  });
+
+//Lancement de l'application
+server.listen(port, () => { console.log('server started' + port) })
