85d7f9319d9d33430e1857db9254eacc31535e20
Alexandre Coutant <alexcoutant3@gmail.com>
2020-03-24 17:41:59 +0100

    connect users, style , event disconnect, response server,

--- app.js
+++ app.js
@@ -5,21 +5,25 @@ let socketio = require('socket.io')
 let morgan = require('morgan')
 let config = require('./config')
 
-//variable globale
+//Constantes
 const app = express()
 const server = http.Server(app)
 const io = socketio(server)
 const port = config.express.port
 const options = {
-    root: __dirname+ '/views'
+    root: __dirname + '/views'
 }
+
+//Variables globales
+let usernames = []
+
 //middlewares
 app.use(express.static(options.root))
 app.use(morgan('dev'))
 
 //Routes
 app.get('/', (req, res) => {
-  res.redirect('./home')
+    res.redirect('./home')
 })
 
 app.get('/home', (req, res) => {
@@ -31,19 +35,59 @@ app.get('/params/:name', (req, res) => {
 })
 
 //IO
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
 
     console.log('a user connected : ' + socket.id);
 
-    setTimeout(() =>{
-        socket.emit('salut')
-    }, 1000);
+    //traitement pour l'assignation d'un username
+    socket.on('setUsername', (usernameWanted) => {
 
-//Deconnexion de l'utilisateur
+        //traitement pour l'assignation d'un username
+        usernameWanted = usernameWanted.trim()
+
+        //verification de l'unicité de l'user
+        let usernameTaken = false
+        for (let socketid in usernames) {
+            if (username[socketid] === usernameWanted)
+            usernameTaken = true
+        }
+
+        let timeFakeLoading = 1000
+        setTimeout(() => {
+
+            //traitement final
+            if (usernameTaken) {
+                socket.emit('rejectUsername', usernameWanted)
+            } else {
+                socket.join('users', () => {
+                    usernames[socket.id] = usernameWanted
+                    socket.emit('acceptUsername', usernameWanted, getUsernames())
+                })
+                
+            }
+        }, timeFakeLoading)
+
+    })
+
+    //Deconnexion de l'utilisateur
     socket.on('disconnect', () => {
         console.log('disconnect : ' + socket.id)
+        if(usernames[socket.id]){
+            delete usernames[socket.id]
+            console.log('username deleted')
+        }
     })
-  });
+});
 
 //Lancement de l'application
 server.listen(port, () => { console.log('server started' + port) })
+
+//renvoi uniquement un array avec les usernames sans index
+
+function getUsernames() {
+    let users = []
+    for (let socketid in usernames){
+        users.push(usernames[socketid])
+    }
+    return users
+}
